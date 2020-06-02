2eb10790196755b8556385a650e5ce53961ab2e7
atMen <377044412@qq.com>
2020-06-03 02:50:31 +0800

    修改test2文件

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       //dev2新增功能
+       //dev2新增功能；；；；；；
     }
     
     
